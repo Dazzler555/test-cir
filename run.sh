f64d8090fd4283049cb7967837aba9683ba3ce1e
dumble555 <>
2023-04-28 04:17:02 +0530

    cir6

--- run.sh
+++ run.sh
@@ -1,13 +1,13 @@
 export DEBIAN_FRONTEND="noninteractive"
-apt update -y
-
-apt install tzdata -y ENV TZ="America/New_York"
-apt install aria2 p7zip-full -y
-apt install git  python3 python3-pip curl mediainfo ffmpeg wget mkvtoolnix -y
-pip3 install yt-dlp
-wget -q https://github.com/donwa/gclone/releases/download/v1.51.0-mod1.3.1/gclone_1.51.0-mod1.3.1_Linux_x86_64.gz
-7z x gclone_1.51.0-mod1.3.1_Linux_x86_64.gz > /dev/null
-chmod a+x ./gclone && mv ./gclone /usr/bin/
+#apt update -y
+#
+#apt install tzdata -y ENV TZ="America/New_York"
+#apt install aria2 p7zip-full -y
+#apt install git  python3 python3-pip curl mediainfo ffmpeg wget mkvtoolnix -y
+#pip3 install yt-dlp
+#wget -q https://github.com/donwa/gclone/releases/download/v1.51.0-mod1.3.1/gclone_1.51.0-mod1.3.1_Linux_x86_64.gz
+#7z x gclone_1.51.0-mod1.3.1_Linux_x86_64.gz > /dev/null
+#chmod a+x ./gclone && mv ./gclone /usr/bin/
 wget -q $cu
 wget -q $yt
 pwd
@@ -20,7 +20,7 @@ else
   final="$video_id+$audio_id"
 fi
 
-yt-dlp --merge-output-format mkv -f "$final" --downloader aria2c -N 10 "$url" --embed-subs -o "$name-${video}p.mkv"
+yt-dlp --merge-output-format mkv -f "$final" --downloader aria2c -N 10 --embed-subs -o "$name-${video_id}p.mkv" "$url" 
 # gclone --config ./rclone.conf move "$name-${video}p.mkv" severus:{$id} -drive-chunk-size 128M -P
 
 # yt-dlp "$sub_url" -o "sub-test.%(ext)s"
