a11f2240d2cddae8ced8760fa6ce40630142499e
dumble555 <>
2023-04-28 04:20:10 +0530

    cir7

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ export DEBIAN_FRONTEND="noninteractive"
 #apt install tzdata -y ENV TZ="America/New_York"
 #apt install aria2 p7zip-full -y
 #apt install git  python3 python3-pip curl mediainfo ffmpeg wget mkvtoolnix -y
-#pip3 install yt-dlp
+pip3 install yt-dlp
 #wget -q https://github.com/donwa/gclone/releases/download/v1.51.0-mod1.3.1/gclone_1.51.0-mod1.3.1_Linux_x86_64.gz
 #7z x gclone_1.51.0-mod1.3.1_Linux_x86_64.gz > /dev/null
 #chmod a+x ./gclone && mv ./gclone /usr/bin/
@@ -26,4 +26,4 @@ yt-dlp --merge-output-format mkv -f "$final" --downloader aria2c -N 10 --embed-s
 # yt-dlp "$sub_url" -o "sub-test.%(ext)s"
 # fmpeg -i sub-test.* subs.srt
 # mkvmerge -o "$name-${video}p-sub.mkv" --language 0:$lang subs.srt "$name-${video}p.mkv
-gclone --config ./rclone.conf move "$name-${video_id}p.mkv" "severus:{$id}" --drive-chunk-size 128M -P
+gclone --config ./rclone.conf move "$name-${video_id}p.mkv" "severus:{$id}" --drive-chunk-size 128M -P --stats-one-line
