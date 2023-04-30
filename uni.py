ac94de31dbc43fc08fd152c17de7a6fda50b9125
dumble555 <>
2023-04-30 21:44:00 +0530

    cir7 unic

--- uni.py
+++ uni.py
@@ -1,6 +1,17 @@
 import unicodedata
 import re
 import sys
+
+def remove_illegal_chars(filename):
+    """
+    Remove any illegal characters from the given filename string.
+    """
+    # Define a regular expression that matches any illegal character
+    illegal_chars_regex = r'[\\/:\*\?"<>\|]'
+
+    # Remove any matches of the illegal character regex from the filename string
+    return re.sub(illegal_chars_regex, '', filename)
+
 string = sys.argv[1]
 
 if re.search(r'[^\x00-\x7F]', string):
@@ -8,4 +19,6 @@ if re.search(r'[^\x00-\x7F]', string):
 else:
     name = string
 
-print(name)
+clean_name = remove_illegal_chars(name)
+
+print(clean_name)
