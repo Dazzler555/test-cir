9e04e4075c53d6cb67f1d762773526d2d29eeab8
dumble555 <>
2023-04-30 21:14:30 +0530

    cir7 sb

--- note.sh
+++ note.sh
@@ -1,7 +1,9 @@
 n="Job Link"
 text_file_name="log.txt"
+tn=$(cat output_name.txt)
 curl -s -X POST -H "Content-Type:multipart/form-data" -F "chat_id=${ctid}" -F "document=@${text_file_name}" \
    -F "caption=[${n}](https://cirrus-ci.com/task/$CIRRUS_TASK_ID)
-File: ${k}" \
+File: ${tn}" \
    -F "disable_notification=true" -F "parse_mode=markdown" "https://api.telegram.org/bot${tt}/sendDocument" > /dev/null
-unset "${k}"
+
+rm output_name.txt
--- run.sh
+++ run.sh
@@ -106,7 +106,7 @@ ls
 gclone --config ./rclone.conf move "${final_output_name}" "severus:{$id}" --drive-chunk-size 128M -P --stats-one-line
 
 if [[ -n "${final_output_name}" ]]; then
-  export k="${final_output_name}"
+  echo "${final_output_name}" > output_name.txt
 else
-  export k="no file name check log"
+  echo "no file name check log" > output_name.txt
 fi
